d7f7f87c41980aa2f77f3b92eafc3d84f7111267
DevilsElbow <clarissa.barks@gmail.com>
2017-01-04 12:31:58 -0600

    Delete snipcart.js

--- snipcart.js
+++
@@ -1,5 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-
-<script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" id="snipcart" data-api-key="MWRkNDhhMmEtMzEwZS00MTZjLThkOTUtNDQ2YmFmZWU5OGQ0NjM2MDM4NzQ3OTg0NDgwMjYx"></script>
-
-<link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" type="text/css" rel="stylesheet" />
