f2b37afd0002f4bb26cc1183df69d7f21cefb89b
DevilsElbow <clarissa.barks@gmail.com>
2017-01-04 12:28:24 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Devils Elbow Art Collective Home</title>
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+
+<link rel="stylesheet" type="text/css" href='css/main.css'>
+
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Brawler" rel="stylesheet">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<?php include("navbar.php");
+ ?>
+
+
+</head>
+
+<body>
+<div id="fb-root"></div>
+
+<section class="jumbotron">
+<div class="container">
+<div class="row text-center">
+
+<h2>Handcrafted in the Ozarks</h2>
+</div>
+</div>
+</section>
+<section class="container">
+	<div class="row">
+		<figure class="col-sm-6">
+		<a href="tiedye.php"<p>Tie Dye</p>
+		<img src="img/turquoise_and_purple.JPG">
+		</figure>
+		</a>
+		
+
+		<figure class="col-sm-6">
+		<a href="jewelry.php"<p>Handmade Jewelry</p>
+		<img src="img/black_yellow_green_herringbone_bracelet.jpg">
+		</figure>
+		</a>
+
+	</div>
+	<div class="fb-like" data-href="https://www.facebook.com/devilselbowart" data-width="200" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+
+
+<?php include("footer.php"); ?>
+
+</body>
+</html>
